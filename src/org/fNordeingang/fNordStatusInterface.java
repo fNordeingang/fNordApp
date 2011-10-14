da68c5269f2f16ec8a0ec588c2c7f8937c3567c0
lcb01 <lcb01@fnordeingang.de>
2011-10-14 02:41:18 +0200

    Changed setFnordStatus funktion to send proper JSON

--- src/org/fNordeingang/fNordStatusInterface.java
+++ src/org/fNordeingang/fNordStatusInterface.java
@@ -7,15 +7,19 @@ package org.fNordeingang;
 //http
 import java.io.IOException;
 
+
 import org.apache.http.impl.client.*;
 import org.apache.http.client.*;
-import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import android.util.Log;
+
 import de.mastacode.http.Http;
+
 
 public final class fNordStatusInterface {
 	
@@ -48,24 +52,34 @@ public final class fNordStatusInterface {
 	
 	public static int setfNordStatus(final String username, final String password) {
 		try {
-			JSONObject userdata = new JSONObject().put("username",username).put("password", password);
-			String toggle = new JSONObject().put("user", userdata).toString();
+			JSONObject userdata = new JSONObject().put("username", username).put("password", password);
+			Log.v("Data:",userdata.toString());		
 			
-			Log.v("String:",toggle);
-			HttpClient client = new DefaultHttpClient();
-			String response = Http.post("http://services.fnordeingang.de/services/api/status").data("jsondata", toggle).use(client).asString();
-			
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+			HttpPost httpost = new HttpPost("http://services.fnordeingang.de/services/api/status");
+					
+			StringEntity se = new StringEntity(userdata.toString());
+			httpost.setEntity(se);
+			httpost.setHeader("Accept", "application/json");
+			httpost.setHeader("Content-type", "application/json");
+
+			ResponseHandler responseHandler = new BasicResponseHandler();
+			String response = httpclient.execute(httpost, responseHandler);
+
+			Log.v("Response:",response);
 			// get status
 			// if this throws a JSONException - no json object returned
 			// => maybe wrong password
 			JSONObject status = new JSONObject(response);
 			
 		} catch (IOException ioe) {
-			//print(ioe.toString());
+			Log.v("IOE: ", ioe.toString());
 			return -1;
 		} catch (JSONException jsone) {
-			//print("Wrong Password?");
+			Log.v("JSONe: ", jsone.toString());
 			return 0;
+		} catch (Exception e) {
+			return -2;
 		}
 		return 1;
 		
