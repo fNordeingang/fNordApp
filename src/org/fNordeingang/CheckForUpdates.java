e4c225ce099714e046e094e3c89c977b324d93d6
Tristan Leo <mail@tristan-leo.de>
2011-10-22 15:06:16 +0200

    updated autoupdater to use git apk

--- src/org/fNordeingang/CheckForUpdates.java
+++ src/org/fNordeingang/CheckForUpdates.java
@@ -25,19 +25,19 @@ import java.io.IOException;
 // http
 
 public class CheckForUpdates {
-	
+
 	ProgressDialog progress;
 	Context context;
-	
+
 	CheckForUpdates(Context context) {
 		this.context = context;
 	}
-	
+
 	public void check() {
 		DownloadServerVersionThread dsvt = new DownloadServerVersionThread(handler);
 		dsvt.start();
 	}
-	
+
 	// Define the Handler that receives messages from the DownloadServerVersionThread and proofs for updates
     final Handler handler = new Handler() {
 		public void handleMessage(Message msg) {
@@ -45,15 +45,15 @@ public class CheckForUpdates {
 			try {
 				PackageInfo versionInfo = context.getPackageManager().getPackageInfo("org.fNordeingang", 0);
 				int localVersion = versionInfo.versionCode;
-	
+
 				int serverVersion = msg.getData().getInt("serverVersion");
-	
+
 				// compare versions
 				if (localVersion > 0 && serverVersion > localVersion) {
 					// newer version available:
 					downloadLatestVersion();
 				}
-	
+
 			} catch (PackageManager.NameNotFoundException e) {
 				// nothing to be done
 			}
@@ -63,25 +63,25 @@ public class CheckForUpdates {
 	// Nested class that downloads the serverVersion information
 	private class DownloadServerVersionThread extends Thread {
 		Handler handler;
-		
+
 		DownloadServerVersionThread(Handler h) {
 			handler = h;
 		}
-		
+
 		public void run() {
-			
+
 			try {
 				// get version from server
 				HttpClient client = new DefaultHttpClient();
 				int serverVersion = Integer.parseInt(Http.get("http://dl.dropbox.com/u/1711476/fNordeingang/fNordApp/latest").use(client).asString().trim());
-				
+
 				// send version to main thread
 				Message msg = handler.obtainMessage();
 				Bundle b = new Bundle();
 				b.putInt("serverVersion", serverVersion);
 				msg.setData(b);
 				handler.sendMessage(msg);
-				
+
 			} catch (IOException e) {
 				// nothing to be done
 			} catch (NumberFormatException e) {
@@ -89,35 +89,35 @@ public class CheckForUpdates {
 			}
 		}
 	}
-	
+
 	private void downloadLatestVersion() {
-		
+
 		// ask user for download
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
 		builder.setTitle("New Version Available");
 		builder.setMessage("Do you want to download the latest version?");
-		
+
 		builder.setCancelable(false);
-		
+
 		// download at yes
 		builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int id) {
-				
+
 				// show a progress
 				progress = ProgressDialog.show(context, "", "Downloading. Please wait...", true, false);
-				
+
 				new Thread() {
 					public void run() {
 						// download url of latest version
 						try {
 							HttpClient client = new DefaultHttpClient();
-							String latestURL = Http.get("http://dl.dropbox.com/u/1711476/fNordeingang/fNordApp/latest_apk").use(client).asString();
-							
+							String latestURL = Http.get("https://raw.github.com/fNordeingang/fNordApp/master/bin").use(client).asString();
+
 							// if there is a previous file - delete it
 							File apk = new File(context.getExternalFilesDir(null), "fNordApp.apk");
 							if (apk.exists())
 								apk.delete();
-							
+
 							// download file
 							java.io.BufferedInputStream in = new java.io.BufferedInputStream(new java.net.URL(latestURL).openStream());
 							java.io.FileOutputStream fos = new java.io.FileOutputStream(apk);
@@ -129,17 +129,17 @@ public class CheckForUpdates {
 							}
 							bout.close();
 							in.close();
-							
+
 							// don't display progress anymore
 							progress.dismiss();
-							
+
 							// install apk
 							android.net.Uri uri = android.net.Uri.fromFile(apk);
-							
+
 							Intent intent = new Intent(Intent.ACTION_VIEW);
 							intent.setDataAndType(uri, "application/vnd.android.package-archive");
 							context.startActivity(intent);
-							
+
 						}
 						catch (IOException e) {
 							print("Error downloading");
@@ -149,24 +149,24 @@ public class CheckForUpdates {
 				}.start();
 			}
 		});
-		
+
 		// cancel dialog at no
 		builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int id) {
 				dialog.cancel();
 			}
 		});
-		
+
 		// display dialog
 		AlertDialog dialog = builder.create();
 		dialog.show();
 	}
-	
+
 	// helper function
 	void print(String input) {
         CharSequence text = input;
         int duration = Toast.LENGTH_SHORT;
-		
+
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
     }
